948acb1827cb5bc3a66b3ac57961d5d8bc34e9bd
Roel Baardman <roel.baardman@gmail.com>
2017-11-04 18:03:43 +0100

    Use new location of plot definition

--- code/plot.sh
+++ code/plot.sh
@@ -3,7 +3,7 @@ END=`date "+%s"`
 BEGIN=`date "+%s" -d '3 days ago -1 hour'`
 #BEGIN=`date "+%s" -d '-1 hour'`
 TEMPFILE=`mktemp`
-cd /home/archive/data/bl3p
+cd /home/roeles/projects/bl3p/plots
 gnuplot -e "set datafile separator \",\"; set grid; set terminal png size 1920,1080; set output \"$TEMPFILE\"; t0=$BEGIN; t1=$END" bl3p.gnuplot
 mv $TEMPFILE $1
 chmod a+rx $1
